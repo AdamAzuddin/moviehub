e8a023f5f05d094f0a6302812248985d384c63ed
Adam Azuddin <05adamaz@gmail.com>
2024-09-03 10:45:50 +0800

    added sign in and sign out button on profile page

--- app/(home)/profile/page.tsx
+++ app/(home)/profile/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/firebase";
+import { User } from "firebase/auth";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const ProfilePage = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [profilePic, setProfilePic] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe(); // Cleanup the subscription on unmount
+  }, []);
+
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      console.log("User signed out successfully");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      {user ? (
+        <Button variant="secondary" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      ) : (
+        <Link href={"/auth"}>
+          <Button variant="outline">Sign In</Button>
+        </Link>
+      )}
+    </div>
+  );
+};
 
-export default page
+export default ProfilePage;
