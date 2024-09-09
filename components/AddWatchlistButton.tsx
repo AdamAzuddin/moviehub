7460ee8de524e072ee9eba24579964628829bd48
Adam Azuddin <05adamaz@gmail.com>
2024-09-09 11:43:24 +0800

    added test for adding and removing from watchlist

--- components/AddWatchlistButton.tsx
+++ components/AddWatchlistButton.tsx
@@ -5,7 +5,6 @@ import { db } from "@/lib/firebase";
 import {
   updateDoc,
   arrayUnion,
-  arrayRemove,
   collection,
   query,
   where,
@@ -18,9 +17,13 @@ interface AddWatchlistButtonProps {
   mediaType: "movie" | "tv";
 }
 
-const AddWatchlistButton: React.FC<AddWatchlistButtonProps> = ({ id, mediaType }) => {
-  const [error, setError] = useState("")
-  const { user } = useAuth(); // Get the current authenticated user
+const AddWatchlistButton: React.FC<AddWatchlistButtonProps> = ({
+  id,
+  mediaType,
+}) => {
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+  const { user } = useAuth();
   const watchlist = useStore((state) => state.watchlist);
   const addToWatchlist = useStore((state) => state.addToWatchlist);
 
@@ -29,25 +32,26 @@ const AddWatchlistButton: React.FC<AddWatchlistButtonProps> = ({ id, mediaType }
       console.log("No user is logged in.");
       return;
     }
-  
+
     try {
       // Query to find the document where the `uid` field matches the user's UID
       const usersCollection = collection(db, "users");
       const q = query(usersCollection, where("uid", "==", user.uid));
       const userSnapshot = await getDocs(q);
-  
+
       if (!userSnapshot.empty) {
         // Assuming there's only one document per user
         const userDocRef = userSnapshot.docs[0].ref;
-  
+
         // Create an item object with movieId and type
         const item = { id, mediaType };
-  
+
         // Check if the item is already in the watchlist
         const isInWatchlist = watchlist.some(
-          (watchlistItem) => watchlistItem.id === id && watchlistItem.mediaType === mediaType
+          (watchlistItem) =>
+            watchlistItem.id === id && watchlistItem.mediaType === mediaType
         );
-  
+
         if (isInWatchlist) {
           setError("This movie/TV show is already in your watchlist.");
         } else {
@@ -55,7 +59,12 @@ const AddWatchlistButton: React.FC<AddWatchlistButtonProps> = ({ id, mediaType }
           await updateDoc(userDocRef, {
             watchlist: arrayUnion(item),
           });
-          addToWatchlist(item); // Pass the entire item object
+          addToWatchlist(item);
+          setError(null)
+          setSuccess("Item added to watchlist!");
+          setTimeout(() => {
+            setSuccess(null);
+          }, 3000);
         }
       } else {
         console.error("User data not found in Firestore");
@@ -64,19 +73,17 @@ const AddWatchlistButton: React.FC<AddWatchlistButtonProps> = ({ id, mediaType }
       console.error("Error updating watchlist:", error);
     }
   };
-  
 
   return (
     <div>
       <button
-        className="bg-blue-600 text-white p-3 rounded-full shadow-md hover:bg-blue-700 transition"
+        className="bg-blue-600 text-white p-3 rounded-full shadow-md hover:bg-blue-700 transition cypress-add-to-watchlist"
         onClick={handleClick}
       >
         <Plus className="w-4 h-4" />
       </button>
-      {error && (
-        <p className="text-red-500 mt-2">{error}</p>
-      )}
+      {error && <p className="text-red-500 mt-2">{error}</p>}
+      {success && <p className="text-green-500 mt-2">{success}</p>}
     </div>
   );
 };
--- cypress/e2e/watchlist.spec.ts
+++ cypress/e2e/watchlist.spec.ts
@@ -1,20 +1,57 @@
-describe('Watchlist Functionality', () => {
-    it('should add and remove a movie from watchlist', () => {
-      // Sign in
-      cy.visit('/auth/sign-in');
-      cy.get('input[name="email"]').type('user@example.com');
-      cy.get('input[name="password"]').type('password');
-      cy.get('button').contains('Sign In').click();
-  
-      // Navigate to movie details
-      cy.visit('/movie-details/1'); // Adjust movie ID as needed
-      cy.get('button').contains('Add to Watchlist').click();
-      cy.visit('/watchlist');
-      cy.contains('Inception').should('be.visible'); // Adjust movie name as needed
-  
-      // Remove from watchlist
-      cy.get('button').contains('Remove from Watchlist').click();
-      cy.contains('Inception').should('not.exist');
-    });
+describe("Sign in Functionality", () => {
+  it("should sign in using an account and redirect to the home page", () => {
+    cy.visit("/auth");
+    cy.get('input[id="email"]').type("max@gmail.com");
+    cy.get('input[id="password"]').type("test1234");
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should("eq", Cypress.config().baseUrl + "/");
   });
-  
+});
+
+describe("Profile Page Functionality", () => {
+  it("should visit profile page and see sign out button", () => {
+    cy.visit("/profile");
+    cy.get("button").contains("Sign Out").should("be.visible");
+  });
+});
+
+describe("Add to Watchlist Functionality", () => {
+  it("should add a movie to watchlist and display a success message", () => {
+    cy.visit("/details/movie/1022789");
+    cy.get("button.cypress-add-to-watchlist").should("be.visible").click();
+    cy.contains("Item added to watchlist!").should("be.visible");
+  });
+});
+
+describe("Confirmation Dialog Functionality", () => {
+  it("should open and close the confirmation dialog", () => {
+    // Navigate to the movie details page and add the movie to watchlist
+    cy.visit("/details/movie/1022789");
+    cy.get("button.cypress-add-to-watchlist").click();
+    cy.visit("/watchlist");
+
+    // Open the confirmation dialog
+    cy.get("button.cypress-remove").click();
+    cy.get(".cypress-alert-dialog-title").contains("Are you sure?");
+
+    // Click "Cancel" and verify item is still in watchlist
+    cy.get(".cypress-alert-dialog-cancel").click();
+    cy.contains("Inside Out 2").should("be.visible");
+  });
+});
+
+describe("Remove from Watchlist Functionality", () => {
+  it("should remove a movie from watchlist after confirming", () => {
+    // Navigate to the movie details page and add the movie to watchlist
+    cy.visit("/details/movie/1022789");
+    cy.get("button.cypress-add-to-watchlist").click();
+    cy.visit("/watchlist");
+
+    // Open the confirmation dialog
+    cy.get("button.cypress-remove").click();
+    cy.get(".cypress-alert-dialog-action").contains("Continue").click();
+
+    cy.contains("Inside Out 2").should("not.exist");
+  });
+});
